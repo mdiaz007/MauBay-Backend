317e6f2c346e90addb150497a9e8665e87bf0b70
Mauricio Diaz <mauricio809diaz@gmail.com>
2025-03-01 00:36:59 -0500

    populated fields for the serializers

--- api/serializers.py
+++ api/serializers.py
@@ -7,25 +7,25 @@ from rest_framework import serializers
 class ActiveListingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = active
-        Fields = []
+        fields = ['id', 'user_id', 'title', 'price', 'post_date', 'description', 'quanitity', 'image_url', 'category', 'condition', 'listing_type']
 
 # Private // May not include
 # Serializer for the deleted model, containing deleted listings.
 class DeletedListingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = deleted
-        Fields = []
+        fields = ['id', 'user_id', 'title', 'price', 'post_date', 'description', 'quanitity', 'image_url', 'category', 'condition', 'listing_type']
 
 # Private, attached to user.
 # Serializer for the draft model, containing drafted listings.
 class DraftListingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = draft
-        Fields = []
+        fields = ['id', 'user_id', 'title', 'price', 'post_date', 'description', 'quanitity', 'image_url', 'category', 'condition', 'listing_type']
 
 # Public
 # Serializer for the sold model, containing sold listings.
 class SoldListingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = sold
-        Fields = []
+        fields = ['id', 'user_id', 'title', 'price', 'post_date', 'description', 'quanitity', 'image_url', 'category', 'condition', 'sell_price', 'sold_date', 'listing_type']
